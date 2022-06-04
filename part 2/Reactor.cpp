81678f6cb3b954717599a597f0de3d0c0a7b2731
Eyal Hagai <eyalh2001@gmail.com>
2022-06-04 16:50:04 +0300

    lets finish this crap

--- part 2/Reactor.cpp
+++ part 2/Reactor.cpp
@@ -68,18 +68,9 @@ void removeHandler(Reactor *reactor, int fd)
 
 #define BACKLOG 10 // how many pending connections queue will hold
 
-void sigchld_handler(int s)
-{
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
+int fd_count = 0;
 
-    while (waitpid(-1, NULL, WNOHANG) > 0)
-        ;
-
-    errno = saved_errno;
-}
-
-// get sockaddr, IPv4 or IPv6:
+// Get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET)
@@ -90,155 +81,204 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-void hello(int fd)
+// Return a listening socket
+int get_listener_socket(void)
 {
-    char buff[1024];
-    if (recv(fd, buff, 1024, 0) == -1)
-    {
-        perror("recieve");
-    }
-    if (write(fd, "Hello, world!", 13) == -1)
-    {
-        perror("send");
-    }
-}
-
-void nothing(int fd)
-{
-    char buff[1024];
-    if (read(fd, buff, 1024) == -1)
-    {
-        perror("recieve");
-    }
-}
-
-void *handle_client(void *args)
-{
-    Reactor *reactor = ((Singleton<Reactor> *)args)->get_data();
-    while (1)
-    {
-        size_t size = reactor->fds.size();
-        int num_events = poll((pollfd *)&(reactor->fds.at(0)), size, -1);
-        printf("num events: %d\n", num_events);
-
-        for (size_t i = 0; i < reactor->fds.size(); ++i)
-        {
-            int pollin_happened = reactor->fds.at(i).revents & POLLIN;
-
-            if (pollin_happened)
-            {
-                printf("File descriptor %d is ready to read\n", reactor->fds.at(i).fd);
-                reactor->funcs.at(i)(reactor->fds.at(i).fd);
-            }
-        }
-    }
-    char *ref = "closed thread";
-    pthread_exit(&ref);
-}
-
-int main()
-{
-    int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes = 1;
-    char s[INET6_ADDRSTRLEN];
+    int listener; // Listening socket descriptor
+    int yes = 1;  // For setsockopt() SO_REUSEADDR, below
     int rv;
 
+    struct addrinfo hints, *ai, *p;
+
+    // Get us a socket and bind it
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+    hints.ai_flags = AI_PASSIVE;
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0)
     {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+        fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+        exit(1);
     }
 
-    // loop through all the results and bind to the first we can
-    for (p = servinfo; p != NULL; p = p->ai_next)
+    for (p = ai; p != NULL; p = p->ai_next)
     {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1)
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (listener < 0)
         {
-            perror("server: socket");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                       sizeof(int)) == -1)
-        {
-            perror("setsockopt");
-            exit(1);
-        }
+        // Lose the pesky "address already in use" error message
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0)
         {
-            close(sockfd);
-            perror("server: bind");
+            close(listener);
             continue;
         }
 
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(ai); // All done with this
 
+    // If we got here, it means we didn't get bound
     if (p == NULL)
     {
-        fprintf(stderr, "server: failed to bind\n");
-        exit(1);
+        return -1;
     }
 
-    if (listen(sockfd, BACKLOG) == -1)
+    // Listen
+    if (listen(listener, 10) == -1)
     {
-        perror("listen");
-        exit(1);
+        return -1;
     }
 
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+    return listener;
+}
+
+void handle_client(int fd)
+{
+    char buf[256]; // Buffer for client data
+    Reactor *reactor = Singleton<Reactor>::Instance()->get_data();
+
+    // If not the listener, we're just a regular client
+    int nbytes = recv(fd, buf, sizeof buf, 0);
+
+    int sender_fd = fd;
+
+    if (nbytes <= 0)
     {
-        perror("sigaction");
-        exit(1);
-    }
-
-    printf("server: waiting for connections...\n");
-
-    FILE *start = fopen("start.txt", "w+");
-    pthread_t thr;
-    Singleton<Reactor> *reactor = (Singleton<Reactor> *)newReactor();
-    int t_start = 0;
-
-    while (1)
-    { // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1)
+        // Got error or connection closed by client
+        if (nbytes == 0)
         {
-            perror("accept");
-            continue;
+            // Connection closed
+            printf("pollserver: socket %d hung up\n", sender_fd);
+        }
+        else
+        {
+            perror("recv");
         }
 
-        installHandler(reactor->get_data(), &hello, new_fd);
-        if (!t_start)
+        close(fd); // Bye!
+
+        removeHandler(reactor, fd);
+        fd_count--;
+    }
+    else
+    {
+        // We got some good data from a client
+
+        for (int j = 0; j < fd_count; j++)
         {
-            pthread_create(&thr, NULL, handle_client, newReactor());
-            t_start = 1;
+            // Send to everyone!
+            int dest_fd = reactor->fds.at(j).fd;
+
+            // Except the listener and ourselves
+            if (dest_fd != reactor->fds.at(0).fd && dest_fd != sender_fd)
+            {
+                if (send(dest_fd, buf, nbytes, 0) == -1)
+                {
+                    perror("send");
+                }
+            }
         }
+    }
+}
 
-        // fputs("hi there", start);
+void accept_client(int fd)
+{
+    // If listener is ready to read, handle new connection
 
-        inet_ntop(their_addr.ss_family,
-                  get_in_addr((struct sockaddr *)&their_addr),
-                  s, sizeof s);
-        printf("server: got connection from %s\n", s);
+    struct sockaddr_storage remoteaddr;
+    socklen_t addrlen = sizeof remoteaddr;
+    int newfd = accept(fd,
+                       (struct sockaddr *)&remoteaddr,
+                       &addrlen);
+    char remoteIP[INET6_ADDRSTRLEN];
+
+    if (newfd == -1)
+    {
+        perror("accept");
+    }
+    else
+    {
+        installHandler(Singleton<Reactor>::Instance()->get_data(), &handle_client, newfd);
+        fd_count++;
+
+        printf("pollserver: new connection from %s on "
+               "socket %d\n",
+               inet_ntop(remoteaddr.ss_family,
+                         get_in_addr((struct sockaddr *)&remoteaddr),
+                         remoteIP, INET6_ADDRSTRLEN),
+               newfd);
+    }
+}
+
+// Main
+int main(void)
+{
+    int listener; // Listening socket descriptor
+
+    int newfd;                          // Newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr; // Client address
+    socklen_t addrlen;
+
+    char buf[256]; // Buffer for client data
+
+    char remoteIP[INET6_ADDRSTRLEN];
+
+    // Start off with room for 5 connections
+    // (We'll realloc as necessary)
+    Reactor *reactor = Singleton<Reactor>::Instance()->get_data();
+
+    // Set up and get a listening socket
+    listener = get_listener_socket();
+
+    if (listener == -1)
+    {
+        fprintf(stderr, "error getting listening socket\n");
+        exit(1);
     }
 
+    // Add the listener to set
+    installHandler(reactor, &accept_client, listener);
+    // pfds[0].fd = listener;
+    // pfds[0].events = POLLIN; // Report ready to read on incoming connection
+
+    fd_count = 1; // For the listener
+
+    // Main loop
+    for (;;)
+    {
+        int poll_count = poll(&(reactor->fds.at(0)), fd_count, -1);
+
+        if (poll_count == -1)
+        {
+            perror("poll");
+            exit(1);
+        }
+
+        // Run through the existing connections looking for data to read
+        for (int i = 0; i < fd_count; i++)
+        {
+
+            // Check if someone's ready to read
+            if (reactor->fds.at(i).revents & POLLIN)
+            { // We got one!!
+
+                if (reactor->fds.at(i).fd == listener)
+                {
+                    reactor->funcs.at(i)(reactor->fds.at(i).fd);
+                }
+            }
+            else
+            {
+                reactor->funcs.at(i)(reactor->fds.at(i).fd);
+            } // END handle data from client
+        }     // END got ready-to-read from poll()
+    }         // END looping through file descriptors
+              // END for(;;)--and you thought it would never end!
     return 0;
 }
