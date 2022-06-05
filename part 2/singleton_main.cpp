01cae0c26c08c55ce21b9c12ca2f2603eb106ac4
Yanir <66729230+yanir75@users.noreply.github.com>
2022-06-05 19:45:31 +0300

    Add files via upload

--- part 2/singleton_main.cpp
+++ part 2/singleton_main.cpp
@@ -1,26 +1,25 @@
 #include "Singleton.cpp"
-
 using namespace std;
 
 void write_to_file(const string data, Singleton<ofstream> *single)
 {
+ // guard can be added here   
     const char *write_data = data.c_str();
     ofstream *stream = single->get_data();
-    stream->write(write_data, data.size());
+    *stream << write_data;
 }
 
 int main(int argc, char const *argv[])
 {
-    ofstream stream;
-    stream.open("test.txt", ios::out | ios::in);
+    Singleton <ofstream> * single = Singleton<ofstream>::Instance();
 
     int num = 10;
     thread threads[num];
+    single->get_data()->open("test.txt", ios::out | ios::in);
 
     for (size_t i = 0; i < num; ++i)
     {
         string data = "Thread " + to_string(i) + "\n";
-        Singleton<ofstream> *single = Singleton<ofstream>::Instance(&stream);
         threads[i] = thread(write_to_file, data, single);
     }
 
@@ -28,6 +27,7 @@ int main(int argc, char const *argv[])
     {
         threads[i].join();
     }
+    single->get_data()->close();
 
     return 0;
 }
